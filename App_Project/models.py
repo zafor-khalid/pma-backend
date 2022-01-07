5a0e97dbfd9deb15fc2c81d02ef0180d85be8273
sobujhstu <sobujhd@gmail.com>
2022-01-07 22:25:00 +0600

    Mysql Database Added

--- App_Project/models.py
+++ App_Project/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_title = models.CharField(max_length = 5550)
+    project_description = models.CharField(max_length = 5550)
+
+    def __str__(self):
+        return self.project_title
+
+class Task(models.Model):
+    project_title = models.CharField(max_length = 5550)
+    project_description = models.CharField(max_length = 5550)
--- ProjectManagementApi/settings.py
+++ ProjectManagementApi/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .config import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_DIR= BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -73,12 +74,24 @@ WSGI_APPLICATION = 'ProjectManagementApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': Config.DB_NAME,
+        'USER': Config.DB_USER,
+        'PASSWORD': Config.DB_PASSWORD,
+        'HOST': Config.DB_HOST,
+         'PORT': Config.DB_PORT,
     }
 }
+
 
 
 # Password validation
@@ -116,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT=MEDIA_DIR
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
