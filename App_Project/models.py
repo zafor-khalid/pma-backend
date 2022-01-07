36739d355b403b230107a49dece8615ea47e5f9f
sobujhstu <sobujhd@gmail.com>
2022-01-07 22:41:32 +0600

    project and task table create

--- App_Project/models.py
+++ App_Project/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Project(models.Model):
@@ -9,5 +11,9 @@ class Project(models.Model):
         return self.project_title
 
 class Task(models.Model):
-    project_title = models.CharField(max_length = 5550)
-    project_description = models.CharField(max_length = 5550)
+    project_title = models.ForeignKey(Project, on_delete=models.CASCADE)
+    developer = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length = 5550)
+
+    def __str__(self):
+        return str(self.project_title)
--- ProjectManagementApi/settings.py
+++ ProjectManagementApi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App_Auth',
+    'App_Project'
 ]
 
 MIDDLEWARE = [
